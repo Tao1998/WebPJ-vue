a50039ef74f18777c51ae18698bda83d4fdc1721
Tao1998 <812918003@qq.com>
2021-12-05 22:49:54 +0800

    rename

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,11 +4,11 @@
  * @Author: Bruce
  * @Date: 2021-11-21 13:25:22
  * @LastEditors: Bruce
- * @LastEditTime: 2021-12-05 21:47:56
+ * @LastEditTime: 2021-12-05 22:07:33
 -->
 <template>
     <div class="main_welcome">
-        <h3 >Welcome Administrator!!!</h3>
+        <h3 >Welcome!!!</h3>
     </div>
 </template>
 <script>
